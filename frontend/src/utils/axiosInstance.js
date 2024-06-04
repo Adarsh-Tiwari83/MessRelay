46a7a0944802179e3002256bbecb3cf330ac1acc
Adarsh Tiwari <tadarsh249@gmail.com>
2024-06-04 21:10:28 +0530

    final fix

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance
 const instance = axios.create({
-  baseURL: "http://localhost:3000", // Your base URL here
+  baseURL: process.env.REACT_APP_SERVER, // Your base URL here
 });
 
 export default instance; // Export the instance to use it elsewhere
